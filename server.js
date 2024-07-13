ba9d0e308f1e73a26f58574116a2b6c4117aaf0b
anrichter <anrichter2@gmail.com>
2024-07-13 16:07:42 -0500

    Added a server.js file with basic get routes for navigating between the two pages. Currently the GET route for /api/notes doesn't work to return saved notes to the notes.html page.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => 
+    fs.readFile('./db/db.json', 'utf-8', (err, noteData) => {
+        console.log(noteData);
+        return res.json(noteData);
+    })
+);
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
