0a6dfa9afdae2ac463097ccad262a643fd603ca7
anrichter <anrichter2@gmail.com>
2024-07-13 17:45:59 -0500

    Added app.post method for api/notes that successfully adds new notes to the db.json file. Also fixed bug where the notes html page wasn't displaying the stored notes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -20,10 +21,42 @@ app.get('/notes', (req, res) =>
 
 app.get('/api/notes', (req, res) => 
     fs.readFile('./db/db.json', 'utf-8', (err, noteData) => {
-        console.log(noteData);
-        return res.json(noteData);
+        // console.log('noteData', noteData);
+        return res.json(JSON.parse(noteData));
     })
 );
+
+app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+    
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+                err ? console.error(err): console.log('New note written to ./db/db.json'))
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting new note');
+    }
+});
 
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
